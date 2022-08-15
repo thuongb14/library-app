a73058d0a764e38675ffc4abb0d19356538c9e98
thuongb14 <thuongb14@gmail.com>
2022-08-15 14:54:38 +1000

    Add .

--- app.js
+++ app.js
@@ -2,18 +2,14 @@ const addBookForm = document.querySelector('.form-control')
 const overlay = document.querySelector('.overlay');
 const form = document.querySelector('.book');
 const cardSection = document.querySelector('.card-section');
-const searchInput = document.querySelector('.search-input')
+const searchForm = document.querySelector('.search-input')
+const searchInput = document.getElementById('search')
 
 function Book(title, author, pages, read) {
     this.title = title,
     this.author = author,
     this.pages = pages,
     this.read = read
-}
-
-function addBookToLibrary(title, author, pages, read) {
-    let book = new Book(title, author, pages, read);
-    myLibrary.push(book);
 }
 
 let myLibrary = [];
@@ -27,7 +23,7 @@ function removePopUp() {
     addBookForm.classList.remove('open-form')
     overlay.classList.remove('open-overlay')
 }
-
+// objArray.map(a => a.foo)
 function outerClick() {
     document.addEventListener('click', function(e) {
         if (e.target.classList.contains('overlay')) {
@@ -47,22 +43,27 @@ document.addEventListener('keydown', (event) => {
     }
 })
 
+function addNewBook() {
+    let title = document.querySelector('#title').value;
+    let author = document.querySelector('#author').value;
+    let pages = document.querySelector('#pages').value;
+    let read = document.querySelector('#read').checked;
+
+    let book = new Book(title, author, pages, read);
+    myLibrary.push(book);
+    displayBook(book.title, book.author, book.pages)
+}
+
 //get data from form and push the value into array.
-form.addEventListener('submit', function(e) {
+form.addEventListener('submit', function(e) {;
     e.preventDefault();
-    addBookToLibrary(
-    form.elements['title'].value,  
-    form.elements['author'].value, 
-    form.elements['pages'].value,
-    form.elements['read'].checked) // checked is true, unchecked is false
-    removePopUp()
-    displayBook()
-    // form.reset()
+    addNewBook();
+    removePopUp();
+    form.reset()
 })
 
-
 //Display the newest added book from form input 
-function displayBook() {
+function displayBook(title, author, pages, read) {
 
     //create card and append elements into it.
     let card = document.createElement('div')
@@ -80,13 +81,13 @@ function displayBook() {
     removeButton.textContent = 'Remove'
 
     // display form data on card
-    p1.textContent = myLibrary[myLibrary.length-1].title;
-    p2.textContent = `Author: ${myLibrary[myLibrary.length-1].author}`;
-    p3.textContent = `Pages: ${myLibrary[myLibrary.length-1].pages}`;
-    if (myLibrary[myLibrary.length-1].read === false) {
+    p1.textContent = title;
+    p2.textContent = `Author: ${author}`;
+    p3.textContent = `Pages: ${pages}`;
+    if (read === false) {
         card.style = 'border-color: red';
     }
-    if (myLibrary[myLibrary.length-1].read === false) {
+    if (read === false) {
         readButton.textContent = 'Unread'
         readButton.style = 'background-color: red';
     } else {
@@ -96,7 +97,6 @@ function displayBook() {
     //toggle read button style & change read status in array object
     myLibrary.forEach((book) => {
         if (book.read === false) { 
-          console.log(book.read, book.title)
           card.style = 'border-color: red';
           readButton.textContent = 'Unread'
           readButton.style = 'background-color: red';
@@ -123,37 +123,21 @@ function displayBook() {
 
         //remove book card and book data from myLibrary array object
         removeButton.onclick = function(e) {
+            if(confirm('Are you sure to remove this book?')) {
             e.target.parentElement.remove()
-            myLibrary = myLibrary.filter(book => book.title !== e.target.parentElement.firstChild.textContent)
+            myLibrary = myLibrary.filter(book => book.title !== title)
+            }
         }
+
+        searchInput.addEventListener('keyup', function() {
+            filter = searchInput.value.toLowerCase();
+            if(title.toLowerCase().includes(filter) || author.toLowerCase().includes(filter)) {
+                card.style = 'display: block'
+            } else {
+                card.style = 'display: none'
+            }
+        })
     })
 }
 
 
-//Make search bar work
-
-
-// // get search bar element
-// const searchInput = document.getElementById("search");
-
-// // listen for user events
-// search.addEventListener("keyup", (event) => {
-//     const { value } = event.target;
-    
-//     // get user search input converted to lowercase
-//     const searchQuery = value.toLowerCase();
-    
-//     for (const book of myLibrary) {
-//         // store name text and convert to lowercase
-//         let name = nameElement.textContent.toLowerCase();
-        
-//         // compare current name to search input
-//         if (name.includes(searchQuery)) {
-//             // found name matching search, display it
-//             nameElement.style.display = "block";
-//         } else {
-//             // no match, don't display name
-//             nameElement.style.display = "none";
-//         }
-//     }
-// });
